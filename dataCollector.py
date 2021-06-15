cc003f800bef878bc613b6b63b3d50252716fb45
JMiller449 <74942656+JMiller449@users.noreply.github.com>
2021-06-15 00:06:52 -0500

    camelcase? more like not right

--- dataCollector.py
+++ dataCollector.py
@@ -3,36 +3,41 @@ import csv
 import json
 from datetime import datetime
 import os
+
 connector = Connector()
+
+
 @connector.ws.register('/lol-pre-end-of-game/v1/currentSequenceEvent', event_types=('UPDATE',))
-async def endGame(connection,event):
-    await getEndGame(connection)
-async def getEndGame(connection):
+async def end_game(connection, event):
+    await get_end_game(connection)
+
+
+async def get_end_game(connection):
     end = await connection.request('get', '/lol-pre-end-of-game/v1/currentSequenceEvent')
     end = json.loads(await end.read())
     if end['priority'] == 3:
-        endInfo = await connection.request('get', '/lol-end-of-game/v1/eog-stats-block')
-        endInfo = json.loads(await endInfo.read())
-        await saveEndGame(endInfo)
-async def saveEndGame(game):
-    if os.path.exists(os.getcwd()+"\\Dataset") != True:
-            os.mkdir(os.getcwd()+"\\Dataset")
+        end_info = await connection.request('get', '/lol-end-of-game/v1/eog-stats-block')
+        end_info = json.loads(await end_info.read())
+        await save_end_game(end_info)
+
+
+async def save_end_game(game):
+    if os.path.exists(os.getcwd() + "\\Dataset") != True:
+        os.mkdir(os.getcwd() + "\\Dataset")
     date = str(datetime.now())
-    finalDate = ""
+    final_date = ""
     for character in date:
-        if character == ":" or character ==".":
-            finalDate+="x"
+        if character == ":" or character == ".":
+            final_date += "x"
         elif character == "":
             pass
         else:
-            finalDate+=character
-    with open(os.getcwd()+"\\Dataset\\"+finalDate+".csv", 'a') as dataset:
-        csvwriter  = csv.writer(dataset)
+            final_date += character
+    with open(os.getcwd() + "\\Dataset\\" + final_date + ".csv", 'a') as dataset:
+        csvwriter = csv.writer(dataset)
         for key, value in game.items():
             csvwriter.writerow([key, value])
         dataset.close()
+
+
 connector.start()
-
-    
-
-        

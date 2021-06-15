30b01877207bb258c81c12580eeb61460c44acd0
bossadapt <37967493+bossadapt@users.noreply.github.com>
2021-06-15 00:38:47 -0500

    split to 2 threads

--- leagueDocMain.py
+++ leagueDocMain.py
@@ -1,9 +1,24 @@
 from tkinter import *
 from frameworkGUI import GUI
+import subprocess
+import threading
 
-root = Tk()
 
-gui = GUI(root)
-gui.show_app_buttons()
+def dataCollection():
+    subprocess.call("pythonw.exe dataCollector.py", shell=False)
 
-root.mainloop()
+
+def gui():
+    root = Tk()
+
+    gui = GUI(root)
+    gui.show_app_buttons()
+
+    root.mainloop()
+
+t2 = threading.Thread(target=dataCollection, args=())
+t1 = threading.Thread(target=gui, args=())
+t1.start()
+t2.start()
+t1.join()
+t2.join()

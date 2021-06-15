d65c5ba7c729a93cf44c3c6ec6c704b6804f3650
JMiller449 <74942656+JMiller449@users.noreply.github.com>
2021-06-14 20:48:06 -0500

    nomenclature

--- League DOC.py
+++ League DOC.py
@@ -3,36 +3,40 @@ import importlib
 import os
 import sys
 from functools import partial
-def buttonPressed(button):
-     print(sys.path)
-     mymodule = importlib.import_module(button)
-     mymodule.run(tk)
-def getApps():
-     apps = []
-     files = os.listdir()
-     for file in files:
-          if "." in file:
-               pass
-          else:
-               apps.append(file)
-               path = os.getcwd()+"\\"+file
-               sys.path.append(path)
-               
-     return apps
+
+
+def button_pressed(folder_name):
+    my_module = importlib.import_module(folder_name)
+    my_module.run(tk)
+
+
+def get_apps():
+    apps = []
+    files = os.listdir()
+    for file in files:
+        if "." in file:
+            pass
+        else:
+            apps.append(file)
+            path = os.getcwd()+"\\"+file
+            sys.path.append(path)
+    return apps
+
+
 window = tk.Tk()
 label = tk.Label(text="Apps")
-label.grid(row=0,column=0)
+label.grid(row=0, column=0)
 counter = 1
-for app in getApps():
-     app = str(app)
-     buttonWithArg = partial(buttonPressed,app)
-     button = tk.Button(
-          text= app,
-          width = 20,
-          height = 3,
-          bg = "black",
-          fg = "white",
-          command = buttonWithArg
-    ).grid(row = counter,  column = 0)
-     counter+=1
+for app in get_apps():
+    app = str(app)
+    button_with_arg = partial(button_pressed, app)
+    button = tk.Button(
+         text=app,
+         width=20,
+         height=3,
+         bg="black",
+         fg="white",
+         command=button_with_arg
+    ).grid(row=counter,  column=0)
+    counter += 1
 window.mainloop()
